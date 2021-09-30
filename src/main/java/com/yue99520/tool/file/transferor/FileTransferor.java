541b29402053d569cd671c2eafb0569adb0c53b9
ernie.lee <ernie.lee@ubitus.net>
2021-09-30 14:53:06 +0800

    [init] init spring boot

---
+++ src/main/java/com/yue99520/tool/file/transferor/FileTransferor.java
@@ -0,0 +1,14 @@
+package com.yue99520.tool.file.transferor;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class FileTransferor {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FileTransferor.class, args);
+    }
+}
