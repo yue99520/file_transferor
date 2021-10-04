c3a87c5cba7a5a5aef202a957bbfce4db663af77
ernie.lee <ernie.lee@ubitus.net>
2021-10-04 15:39:26 +0800

    [dev] Set up application maven assembly script

---
+++ bin/run.bat
@@ -0,0 +1,28 @@
+@echo off
+setlocal enabledelayedexpansion
+
+if "%OS%"=="Windows_NT" goto nt
+echo This script only works with NT-based versions of Windows.
+goto :eof
+
+:nt
+
+IF DEFINED JAVA11_HOME (
+  SET JAVA_HOME=%JAVA11_HOME%
+)
+
+if NOT DEFINED JAVA_HOME (
+    echo JAVA_HOME environment variable must be set!
+    goto :eof
+)
+
+set FILE_TRANSFEROR_HOME=%CD%
+set CLASS_PATH="%FILE_TRANSFEROR_HOME%/config";"%FILE_TRANSFEROR_HOME%/lib/*"
+set BOOT_JAR="%FILE_TRANSFEROR_HOME%/file-transferor-boot.jar"
+set JVM_OPT=-server -Xms256m -Xmx256m -Xmn512m^
+    -XX:MetaspaceSize=48m^
+    -XX:MaxMetaspaceSize=256m^
+    -XX:-OmitStackTraceInFastThrow
+
+
+"%JAVA_HOME%\bin\java" -cp %CLASS_PATH% -jar %BOOT_JAR%
--- src/main/java/com/yue99520/tool/file/transferor/FileTransferor.java
+++ src/main/java/com/yue99520/tool/file/transferor/FileTransferor.java
@@ -1,15 +1,25 @@
 package com.yue99520.tool.file.transferor;
 
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.event.ContextRefreshedEvent;
 
 
 @SpringBootApplication
-public class FileTransferor {
+public class FileTransferor implements ApplicationListener<ContextRefreshedEvent> {
 
     public static void main(String[] args) {
         SpringApplication.run(FileTransferor.class, args);
+    }
+
+    private static final Logger logger = LoggerFactory.getLogger(FileTransferor.class);
+
+    @Override
+    public void onApplicationEvent(ContextRefreshedEvent contextRefreshedEvent) {
+        logger.info("Current process id: {}", ProcessHandle.current().pid());
     }
 }
